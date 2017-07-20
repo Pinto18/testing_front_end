5db199ab54323317f6f5c08addea0dc866287f62
Pinto18 <pinto18.nd@gmail.com>
2017-07-20 10:45:52 -0400

    Feature Added TO Navigate To Chunks

--- Integration_Tests.py
+++ Integration_Tests.py
@@ -13,7 +13,7 @@ class AudioFileTests(unittest.TestCase):
     def test_two_audio_files_playing_simultaneously(self):
         self.navigate_to_projects_page()
         self.navigate_to_a_chunk()
-
+        self.play_audio_file()
 
     @classmethod
     def tearDownClass(inst):
@@ -32,13 +32,25 @@ class AudioFileTests(unittest.TestCase):
     #helper method to navigate to the first chunk of a project
     def navigate_to_a_chunk(self):
         try:
-            self.language_filter = self.driver.find_elements_by_class_name("ui active visible search selection dropdown")[0]
-            #self.language_filter.clear()
-            self.language_filter.send_keys("English demo2")
-            self.language_filter.submit()
-        except NoSuchElementException:
-            print("Error with 'Select Language' Filter")
+            self.language_filter = self.driver.find_element_by_xpath(
+                "//*[@id=\"root\"]/div/div[2]/div/div[1]/div[1]/input")
+            self.language_filter.send_keys("English")
+            self.drop_down_item = self.driver.find_element_by_xpath(
+                "//*[@id=\"root\"]/div/div[2]/div/div[1]/div[1]/div[2]/div")
+            self.drop_down_item.click()
+            self.project_selection = self.driver.find_element_by_xpath(
+                "//*[@id=\"root\"]/div/div[2]/div/div[2]/table/tbody/tr")
+            self.project_selection.click()
+            self.chapter_selection = self.driver.find_element_by_xpath(
+                "//*[@id=\"root\"]/div/div[2]/div/div/table/tbody/tr")
+            self.chapter_selection.click()
+            self.chunk_selection = self.driver.find_element_by_xpath(
+                "//*[@id=\"root\"]/div/div[2]/div/div[1]/div/div/div/div[1]")
+            self.chunk_selection.click()
+        except Exception:
+            print("Error while navigating to chunk")
         return True
+    
 
 if __name__ == '__main__':
     unittest.main()
