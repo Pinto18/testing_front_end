ad5d2d9fc18a721a2f18621601922dbaf006f1a9
Pinto18 <pinto18.nd@gmail.com>
2017-07-20 14:18:02 -0400

    Commiting so Ann can merge her branch

--- Integration_Tests.py
+++ Integration_Tests.py
@@ -5,16 +5,17 @@ import unittest
 from helper import *
 
 
-
-
 class AudioFileTests(unittest.TestCase):
     def setUp(inst):
-        inst.driver = webdriver.Chrome('C:/Users/ann_ejones/Documents/8woc2017/node_modules/chromedriver/lib/chromedriver/chromedriver.exe')
+        inst.driver = webdriver.Chrome()
         inst.driver.implicitly_wait(30)
         inst.driver.maximize_window()
         inst.driver.get('localhost:3000')
 
     def test_two_audio_files_playing_simultaneously(self):
+        """Testing that when one audio file is played after another
+        that the first audio file will stop playing when the second
+        starts."""
         navigate_to_projects_page(self.driver)
         navigate_to_a_chunk(self.driver)
 
@@ -32,7 +33,27 @@ class AudioFileTests(unittest.TestCase):
         self.play_button2.click()
 
         #verify that the first audio is paused now that second audio file is playing
-        self.assertTrue(self.isElementPresent("/ *[ @ id = \"PlayBtn-2\"]"))
+        self.assertTrue(isElementPresent(self. driver, "/ *[ @ id = \"PlayBtn-2\"]"))
+
+        #Clean Up Testing Environment
+        self.pause_button1 = self.driver.find_element_by_xpath("//*[@id=\"PauseBtn-2\"]")
+        self.pause_button1.click()
+        self.pause_button2 = self.driver.find_element_by_xpath("//*[@id=\"PauseBtn-2\"]")
+        self.pause_button2.click()
+
+    # def test_that_audio_stops_playing_when_url_changes(self):
+    #     """Test that navigating to a new URL while
+    #     an audio file is playing will cause the ausdio
+    #     file to stop playing"""
+    #     navigate_to_projects_page(self.driver)
+    #     navigate_to_a_chunk(self.driver)
+    #     self.play_button1 = self.driver.find_element_by_xpath("//*[@id=\"PlayBtn-2\"]")
+    #     self.play_button1.click()
+    #     self.projects_button = self.driver.find_element_by_link_text("Projects")
+    #     self.projects_button.click()
+    #     self.assertTrue(!(Boolean.parseboolean(self.play_button1.get_attribute("ended"))))
+
+
 
     @classmethod
     def tearDown(inst):
